98ca7fc23a3fef7a8b8d59f2017814dd33a63e51
Anton Desyatov <anton.dessiatov@gmail.com>
2021-02-19 12:30:32 +0700

    stgit: 0.23 -> 1.0

--- pkgs/applications/version-management/git-and-tools/stgit/default.nix
+++ pkgs/applications/version-management/git-and-tools/stgit/default.nix
@@ -2,13 +2,13 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "stgit";
-  version = "0.23";
+  version = "1.0";
 
   src = fetchFromGitHub {
     owner = "ctmarinas";
     repo = "stgit";
     rev = "v${version}";
-    sha256 = "1r9y8qnl6kdvq61788pnfhhgyv2xrnyrizbhy4qz4l1bpqkwfr2r";
+    sha256 = "16q8994widg040n1ag4m82kbn3r02n39ah7dvwa7aixhw5y35vlm";
   };
 
   nativeBuildInputs = [ installShellFiles ];
